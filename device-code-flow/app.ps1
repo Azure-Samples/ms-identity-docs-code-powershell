930d114fa40a1e9d1c77e106ba5795b8ab0fb3db
Jason Bouska <82831332+skabou@users.noreply.github.com>
2022-02-15 13:00:09 -0800

    Work In Progress
    Updated the code to use MSAL.PS  (just as an example)
    Trying to get it to work without MSAL.PS

--- device-code-flow/app.ps1
+++ device-code-flow/app.ps1
@@ -7,60 +7,22 @@ $ClientId = ""
 # 'Directory (tenant) ID' of app registration in Azure portal - this value is a GUID
 $TenantId = ""
 
-# Configure $DeviceCodeRequestParams for the desired $TenantId and $ClientId
-$DeviceCodeRequestParams = @{
-    Method = "POST"
-    Uri    = "https://login.microsoftonline.com/$TenantId/oauth2/devicecode"
-    Body   = @{
-        resource  = "https://graph.microsoft.com"
-        client_id = $ClientId
-    }
-}
+# Request the token from MSAL using the configured ClientId and TenantId for the "User.Read" scope
+# The -Device flag is used to initiate a Device Code flow
+# $TokenRequest will contain the response of this request
+$TokenRequest = Get-MsalToken -ClientId $ClientId -Authority "https://login.microsoftonline.com/$TenantId" -Scopes "User.Read" -Device
 
-# Initiate a device code flow using the values specified in $DeviceCodeRequestParams
-$DeviceCodeRequest = Invoke-RestMethod @DeviceCodeRequestParams
-
-# Display the message instructing the user to enter a code at the specified URL on their device
-Write-Host $DeviceCodeRequest.message
-
-# Configure $TokenRequestParams for the desired $TenantId
-# The grant_type "urn:ietf:params:oauth:grant-type:device_code" is required for the Device Code flow
-$TokenRequestParams = @{
-    Method = "POST"
-    Uri    = "https://login.microsoftonline.com/$TenantId/oauth2/token"
-    Body   = @{
-        grant_type = "urn:ietf:params:oauth:grant-type:device_code"
-        code       = $DeviceCodeRequest.device_code
-        client_id  = $ClientId
-    }
-}
-
-# Poll once per second to check if the user has successfully authenticated.
-# If authentication is still pending, suppress the "authorization_pending" error message.
-while ([string]::IsNullOrEmpty($TokenRequest.access_token)) {
-    $TokenRequest = try {
-        Invoke-RestMethod @TokenRequestParams -ErrorAction Stop
-    }
-    catch {
-        $Message = $_.ErrorDetails.Message | ConvertFrom-Json
-        if ($Message.error -ne "authorization_pending") {
-            throw
-        }
-    }
-    Start-Sleep -Seconds 1
-}
-
-# Configure $GraphRequestParams with the desired Graph URL and access_token
+# Configure $GraphRequestParams with the AccessToken received from MSAL as the Bearer token for Graph
 $GraphRequestParams = @{
     Method  = "GET"
     Uri     = "https://graph.microsoft.com/v1.0/me"
     Headers = @{
-        "Authorization" = "Bearer " + $TokenRequest.access_token
+        "Authorization" = "Bearer " + $TokenRequest.AccessToken
     }
 }
 
 # Send a request to the Graph API with the token to retrieve the values from /me
 $GraphRequest = Invoke-RestMethod @GraphRequestParams
 
-# Display the output from Graph to the console
+# Display the response to the console
 Write-Output $GraphRequest
